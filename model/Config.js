abb48dd422ab1e35dea7b0805ebbe47e36ecac33
Fikri Rida Pebriansyah <fikriultimate18@gmail.com>
2022-01-22 00:07:08 +0700

    Add timestamp to config model

--- model/Config.js
+++ model/Config.js
@@ -1,17 +1,22 @@
 const mongoose = require("mongoose");
 
-const configSchema = new mongoose.Schema({
-  user_id: String,
-  refresh_time: Number,
-  logging_time: Number,
-  ph: Number,
-  light_intensity: Number,
-  nutrient_flow: Number,
-  tds: Number,
-  ec: Number,
-  day_start: Date,
-  day_end: Date,
-});
+const configSchema = new mongoose.Schema(
+  {
+    user_id: String,
+    refresh_time: Number,
+    logging_time: Number,
+    ph: Number,
+    light_intensity: Number,
+    nutrient_flow: Number,
+    tds: Number,
+    ec: Number,
+    day_start: Date,
+    day_end: Date,
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const Configurations = mongoose.model("configurations", configSchema);
 
